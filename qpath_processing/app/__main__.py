658a09034755e9f3ee6bf174ab62fa6f2e9b90d3
jean <jean.jacquemier@epfl.ch>
2021-10-22 10:59:56 +0200

    Adjust argument parameter

--- qpath_processing/app/__main__.py
+++ qpath_processing/app/__main__.py
@@ -16,14 +16,15 @@ from qpath_processing.version import VERSION
 @click.command()
 @click.option('--config-file-path', required=False, help='Configuration file path')
 @click.option('--cell-position-file_path', help='Cells position file path.', required=False)
-@click.option('--annotations-geojson-path', help='annotations geojson file path.', required=False)
-@click.option('--pixel-file-path', help='pixel size file path.', required=False)
+@click.option('--annotations-geojson-path', help='Annotations geojson file path.', required=False)
+@click.option('--pixel-file-path', help='Pixel size file path.', required=False)
 @click.option('--thickness-cut', default=50, help='The thikness of the cut (default 50 um)')
-@click.option('--nb-slices', default=100, help='Number of slices (default 100)')
-@click.option('--output-file-path', help='output file path', required=False)
+@click.option('--nb-row', default=100, help='Number of row for the grid (default 100)')
+@click.option('--nb-col', default=100, help='Number of columns for the grid (default 100)')
+@click.option('--output-file-path', help='Output file path', required=False)
 @click.option('--visualisation-flag', is_flag=True)
 def process(config_file_path, cell_position_file_path, annotations_geojson_path, pixel_file_path,
-            thickness_cut, nb_slices, output_file_path, visualisation_flag):
+            thickness_cut, nb_row, nb_col, output_file_path, visualisation_flag):
     if config_file_path:
         config = configparser.ConfigParser()
         config.sections()
@@ -50,7 +51,7 @@ def process(config_file_path, cell_position_file_path, annotations_geojson_path,
     print('INFO: Computes the cells densities as function of percentage depth')
     nb_cell_per_slide = count_nb_cell_per_polygon(cells_centroid_x, cells_centroid_y, split_polygons)
     densities_dataframe = compute_cell_density(nb_cell_per_slide, split_polygons, thickness_cut/1e3)
-    
+
     print('INFO: Write results')
     write_densities_csv(densities_dataframe, output_file_path)
 
