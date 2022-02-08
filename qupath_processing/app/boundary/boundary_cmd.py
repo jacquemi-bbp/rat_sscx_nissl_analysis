086ed8d1e9f34c7b04fd00a21414286bdf38e15b
Jean Jacquemier <jean.jacquemier@epfl.ch>
2022-02-08 16:59:18 +0100

    Display layers boundaries

--- qupath_processing/app/boundary/boundary_cmd.py
+++ qupath_processing/app/boundary/boundary_cmd.py
@@ -65,63 +65,62 @@ def cmd(config_file_path, output_path, visualisation_flag):
         layer_ymin = y_coors[y_coors.argsort()[0:10]].mean()
         y_lines.append(layer_ymin)
         final_result[layer_label] = layer_ymin - y_origin
+        
 
     if visualisation_flag:
 
         x_values = [top_left[0], top_right[0]]
         y_values = [top_left[1], top_right[1]]
 
-        fig, axs = plt.subplots(2, 2, figsize=(8, 8))
-        axs[0][0].invert_yaxis()
-        axs[0][0].plot(x_values, y_values, c='black')
+
+        fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+        axs[0].invert_yaxis()
+        axs[0].plot(x_values, y_values, c='black')
         for XY in layer_points.values():
-            axs[0][0].scatter(XY[:, 0], XY[:, 1], s=2, alpha=0.8)
-        axs[0][0].set_title('Origin points per layer')
+            axs[0].scatter(XY[:, 0], XY[:, 1], s=2, alpha=0.8)
+        axs[0].set_title('Origin points per layer')
 
 
-        axs[0][1].invert_yaxis()
+        axs[1].invert_yaxis()
         for XY in layer_clustered_points.values():
-            axs[0][1].scatter(XY[:, 0], XY[:, 1], s=2, alpha=0.8)
-            axs[0][1].set_title('After removing points outside the clusters')
-            axs[0][1].plot(x_values, y_values, c='black')
+            axs[1].scatter(XY[:, 0], XY[:, 1], s=2, alpha=0.8)
+            axs[1].set_title('After removing points outside the clusters')
+            axs[1].plot(x_values, y_values, c='black')
 
-        axs[1][0].invert_yaxis()
+        axs[2].invert_yaxis()
         x_values = [rotated_points[0][0], rotated_points[1][0]]
         y_values = [rotated_points[0][1], rotated_points[1][1]]
-        axs[1][0].plot(x_values, y_values, c='black')
+        axs[2].plot(x_values, y_values, c='black')
         for XY in layer_rotatated_points.values():
-            axs[1][0].scatter(XY[:, 0], XY[:, 1], s=2, alpha=0.8)
-        axs[1][0].set_title('After clustering and rotation')
+            axs[2].scatter(XY[:, 0], XY[:, 1], s=2, alpha=0.8)
+        axs[2].set_title('After clustering and rotation')
         plt.show()
 
         # Display layer boundaries
+        plt.figure(figsize=(8, 8))
+        plt.gca().invert_yaxis()
         for layer_label, XY in layer_rotatated_points.items():
             plt.scatter(XY[:, 0], XY[:, 1] - y_origin, s=10, alpha=.5)
-            y_coors = XY[:, 1]
-            x_coors = XY[:, 0]
-
-            layer_ymin = y_coors[y_coors.argsort()[0:10]].mean()
-            layer_ymax = y_coors[y_coors.argsort()[-10:-1]].mean()
-            xmean = x_coors.mean()
-            y_lines.append(layer_ymin)
-            plt.hlines(layer_ymin - y_origin, XY[:, 0].min(), XY[:, 0].max(),
+            y = final_result[layer_label]
+            plt.hlines(y, XY[:, 0].min(), XY[:, 0].max(),
                        color='black')
-            final_result[layer_label] = layer_ymin - y_origin
-
         y_lines.append(layer_rotatated_points['Layer 6b'][:, 1].max())
-
         half_letter_size = 10
         y_0 = y_lines[0]
         for y_1, layer_name in zip(y_lines[1:],
                                    ['Layer 1', 'Layer 2', 'Layer 3', 'Layer 4',
                                     'Layer 5',
                                     'Layer 6 a', 'Layer 6b']):
+            x_coors = XY[:, 0]
+            xmean = x_coors.mean()
             y = (y_0 + y_1) / 2
             plt.text(xmean, y - y_origin + half_letter_size, layer_name,
                      size='xx-large')
             y_0 = y_1
 
         plt.show()
+
+
 
 
     print(f'INFO final result {final_result}')
