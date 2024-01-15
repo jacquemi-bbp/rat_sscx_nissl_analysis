d75aa360ce9640ba0265aa69bcf99eff8f0a9d9a
Jean Jacquemier <jean.jacquemier@epfl.ch>
2024-01-15 10:43:24 +0100

    tipo correction

--- qupath_processing/app/exclude/single.py
+++ qupath_processing/app/exclude/single.py
@@ -8,13 +8,13 @@ from qupath_processing.utilities import stereology_exclusion
     "--dataframe-file-path", required=True, help="Full path to the input dataframe"
 )
 @click.option(
-    "--dataframe-output-file-path",
+    "--dataframe-output-path",
     required=True,
-    help="Output directory path to export  the  dataframe with the exclude_flag",
+    help="Output directory path to export the dataframe with the exclude_flag",
 )
 
 def cmd(
-    dataframe_file_path, dataframe_output_file_path):
+    dataframe_file_path, dataframe_output_path):
 
     print(f'INFO: Start cells exclusion')
     cells_features_dataframe = pd.read_csv(dataframe_file_path, index_col=0)
@@ -24,8 +24,8 @@ def cmd(
     #Write Cells featrues dataframe
     slash_pos = dataframe_file_path.rfind('/')
     if slash_pos > -1:
-        dataframe_output_file_path = dataframe_output_file_path[slash_pos + 1:]
+        dataframe_output_file_path = dataframe_output_path[slash_pos + 1:]
     else:
-        dataframe_output_file_path = dataframe_output_file_path
+        dataframe_output_file_path = dataframe_output_path
     print(f'INFO: Export cells features to {dataframe_output_file_path}')
     cells_features_dataframe.to_csv(dataframe_output_file_path)
